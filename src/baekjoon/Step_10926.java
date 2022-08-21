f52283818cd90fbaa7eb6b20f8233784d8d776ed
himdol <ihimchan746@gmail.com>
2022-08-21 17:34:00 +0900

    코딩테스트

---
+++ src/baekjoon/Step_10926.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class Step_10926 {
+	public static void main(String[] args) {
+		String name[] = {"joonas","baekjoon"};
+		
+		for (int i = 0; i<name.length; i++) {
+			System.out.println(name[i]+"??!");
+		}
+	}
+}
---
+++ src/baekjoon/step1.java
@@ -0,0 +1,7 @@
+package baekjoon;
+
+public class step1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
