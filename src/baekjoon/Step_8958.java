b51032140849816730699d87e0aad4fa0870df24
himdol <ihimchan746@gmail.com>
2022-08-29 23:50:12 +0900

    8958 시작

---
+++ src/baekjoon/Step_8958.java
@@ -0,0 +1,45 @@
+package EampleTest.src.baekjoon;
+
+public class Step_8958 {
+    public static void main(String[] args) {
+        String[] n = {"OOXXOXXOOO"
+        ,"OOXXOOXXOO"
+        ,"OXOXOXOXOXOXOX"
+        ,"OOOOOOOOOO"
+        ,"OOOOXOOOOXOOOOX"};
+
+        OOXXOXXOOO
+                OOXXOXXOOO
+        OOXXOXXOOO
+                OXXOXXOOO
+        OOXXOXXOOO
+                XXOXXOOO
+        OOXXOXXOOO
+                XOXXOOO
+        OOXXOXXOOO
+                OXXOOO
+        OOXXOXXOOO
+                XXOOO
+        OOXXOXXOOO
+                XOOO
+        OOXXOXXOOO
+                OOO
+        OOXXOXXOOO
+                OO
+        OOXXOXXOOO
+                O
+
+        for(int i = 0; i < n[0].length(); i++){
+            System.out.println(n[0].substring(i));
+        }
+
+
+        for(String nn : n) {
+            for (int i = 0; i < nn.length(); i++){
+            }
+        }
+
+    }
+}
+
+
