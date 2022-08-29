dfb6ae7de7f7c02a2c5accae990355c82c868712
himdol <ihimchan746@gmail.com>
2022-08-30 01:08:14 +0900

    책점 결과 : 맞았습니다.

--- src/baekjoon/Step_8958.java
+++ src/baekjoon/Step_8958.java
@@ -1,18 +1,21 @@
 package EampleTest.src.baekjoon;
 
+import java.util.Scanner;
+
 public class Step_8958 {
     public static void main(String[] args) {
-        String[] n = {"OOXXOXXOOO"
-                , "OOXXOOXXOO"
-                , "OXOXOXOXOXOXOX"
-                , "OOOOOOOOOO"
-                , "OOOOXOOOOXOOOOX"};
+        Scanner in = new Scanner(System.in);
+        String arr[] = new String[in.nextInt()];
 
-        for (int k = 0; k < n.length; k++) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.next();
+        }
+
+        for (int k = 0; k < arr.length; k++) {
             int j = 0;
             int c = 1;
-            for (int i = 0; i < n[k].length(); i++) {
-                if ("X".equals(n[k].substring(i, i + 1))) {
+            for (int i = 0; i < arr[k].length(); i++) {
+                if ("X".equals(arr[k].substring(i, i + 1))) {
                     c = 1;
                 } else {
                     j += c;
