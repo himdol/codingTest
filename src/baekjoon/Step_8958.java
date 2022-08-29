184ce8f327a77e22058eff264d0f6089444f72a8
himdol <ihimchan746@gmail.com>
2022-08-30 00:24:36 +0900

    8958 풀이 해봐야할 듯

--- src/baekjoon/Step_8958.java
+++ src/baekjoon/Step_8958.java
@@ -3,39 +3,29 @@ package EampleTest.src.baekjoon;
 public class Step_8958 {
     public static void main(String[] args) {
         String[] n = {"OOXXOXXOOO"
-        ,"OOXXOOXXOO"
-        ,"OXOXOXOXOXOXOX"
-        ,"OOOOOOOOOO"
-        ,"OOOOXOOOOXOOOOX"};
+                , "OOXXOOXXOO"
+                , "OXOXOXOXOXOXOX"
+                , "OOOOOOOOOO"
+                , "OOOOXOOOOXOOOOX"};
 
-        OOXXOXXOOO
-                OOXXOXXOOO
-        OOXXOXXOOO
-                OXXOXXOOO
-        OOXXOXXOOO
-                XXOXXOOO
-        OOXXOXXOOO
-                XOXXOOO
-        OOXXOXXOOO
-                OXXOOO
-        OOXXOXXOOO
-                XXOOO
-        OOXXOXXOOO
-                XOOO
-        OOXXOXXOOO
-                OOO
-        OOXXOXXOOO
-                OO
-        OOXXOXXOOO
-                O
+        final String ANSWER_O = "O";
+        final String ANSWER_X = "X";
 
-        for(int i = 0; i < n[0].length(); i++){
-            System.out.println(n[0].substring(i));
+        int j = 0;
+        for (int i = 0; i < n[0].length(); i++) {
+//            System.out.println(n[0].substring(i, i+1));
+//            String now = n[0].substring(i, i+1);
+            if ()
+            int k = 0;
+            k = 1 + i;
+            j += k;
+            System.out.println(k);
         }
+        System.out.println(j);
 
 
-        for(String nn : n) {
-            for (int i = 0; i < nn.length(); i++){
+        for (String nn : n) {
+            for (int i = 0; i < nn.length(); i++) {
             }
         }
 
